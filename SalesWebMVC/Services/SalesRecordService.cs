b0b57ad4238ac0badaa1e1a6102c029a6e051cd7
Kevin Ortega <kevin.a.ortega131@gmail.com>
2024-02-08 23:54:04 -0300

    Implementing group search

--- SalesWebMVC/Services/SalesRecordService.cs
+++ SalesWebMVC/Services/SalesRecordService.cs
@@ -20,11 +20,30 @@
             {
                 result = result.Where(x => x.Date <= maxDate.Value);
             }
-            return result
+            return await result
                 .Include(x => x.Seller)
                 .Include(x => x.Seller.Department)
                 .OrderByDescending(x => x.Date)
-                .ToList();
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Department,SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecords select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            var res = await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+            return res.GroupBy(x => x.Seller.Department).ToList();
         }
     }
 }
